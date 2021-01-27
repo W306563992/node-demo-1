cbd5f19a81566b00808f5324e6b642e8bf0d3e0f
mona <306563992@qq.com>
2021-01-27 21:24:23 +0800

    修改了页面

--- server.js
+++ server.js
@@ -24,7 +24,20 @@ var server = http.createServer(function(request, response){
   if(path === '/'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`<h1> 你好 </h1>`)
+    response.write(`
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="/x">
+</head>
+<body>
+    <h1>你好</h1>
+</body>
+</html>
+    `)
     response.end()
   } else if(path === '/x'){
     response.statusCode = 200
